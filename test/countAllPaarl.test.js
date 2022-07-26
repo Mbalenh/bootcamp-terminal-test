50040fb5b23538b89c1154477869e0475dee6d8c
Mbalenh <3408534@myuwc.ac.za>
2022-07-26 11:32:10 +0200

    added modified files

--- test/countAllPaarl.test.js
+++ test/countAllPaarl.test.js
@@ -2,19 +2,19 @@ let assert = require("assert");
 let countAllPaarl = require("../countAllPaarl");
 
 describe("The countAllPaarl function" , function(){
-it("should return the total of the regNum that startsWith('CJ')", function(){
+it("It should return 2 registrations from CJ if there are three registration numbers from Paarl", function(){
     assert.equal(2, countAllPaarl('CJ 345 123, CK 345, CJ 123'));
 });
-it("should return the total of the regNum that startsWith('CJ')", function(){
+it("It should return 1 registrations from CJ ,if registration numbers from paarl", function(){
     assert.equal(1, countAllPaarl('CK 345 123, CK 345, CJ 123'));
 });
-it("should return the total of the regNum that startsWith('CJ')", function(){
+it("It should return 3 registrations from CJ if there are three registration numbers from Paarl", function(){
     assert.equal(3, countAllPaarl('CJ 345 123, CJ 345, CJ 123'));
 });
-it("shouldreturn the total of the regNum that startsWith('CJ')", function(){
+it("It should return 0 registrations from CJ if there are no registration numbers from Paarl", function(){
     assert.equal(0, countAllPaarl('CK 345 123, CK 345, CK 123'));
 });
-it("should  return the total of the regNum that startsWith('CJ')", function(){
+it("It should return 0 registrations from CJ if there are no registration numbers from Paarl", function(){
     assert.equal(0, countAllPaarl("NUZ 10") );
 });
 

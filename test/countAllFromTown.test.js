073c8fc5b24f23c9fea9716bfc69dfcc34d6aa24
Mbalenh <3408534@myuwc.ac.za>
2022-07-26 13:02:32 +0200

    added modified files

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -2,13 +2,13 @@ let assert = require("assert");
 let countAllFromTown = require("../countAllFromTown");
 
 describe("The countAllFromTown function" , function(){
-    it("It should return 2 registrations from CL if there are three registration numbers from fromstellie", function(){
-        assert.equal(3, countAllFromTown('CL 124,CY 567,CL 345, CJ 456,','CL'));
+    it("It should return 2 registrations from CL if there are two registration numbers from fromstellie", function(){
+        assert.equal(2, countAllFromTown('CL 124,CY 567,CL 345, CJ 456,','CL'));
     });
     it("It should return 3 registrations from CL if there are three registration numbers fromstellie", function(){
         assert.equal(3, countAllFromTown('CL 345 123, CL 345, CL 123','CL'));
     });
-    it("It should return 3 registrations from CA if there are three registration numbers from cape town", function(){
+    it("It should return 1 registrations from CA if there are 1 registration numbers from cape town", function(){
         assert.equal(1, countAllFromTown('CA 345 123, CJ 345, CJ 123','CA'));
     });
     it("It should return 0 registrations from CJ if there are three registration numbers from Cj", function(){
